e296b1ae9615ddbe2d3645ae09af4a2822f1ac09
SagaKuutar <78218119+SagaKuutar@users.noreply.github.com>
2021-02-05 12:36:43 +0200

    hienosaatoa

--- index.php
+++ index.php
@@ -19,11 +19,11 @@
 <br>
 <main>
 <div class="sisalto">
-<h1> Sisältöä </h1>
+<h1> Sisältö </h1>
 
 <form action="index.php" method="post">
-    Tunnus: <input type="tekst" name="tunnus"> <br>
-    Salasana: <input type="password" name="salasana"> <br>
+    Tunnus: <br><input type="tekst" name="tunnus"> <br><br>
+    Salasana: <br><input type="password" name="salasana"> <br><br>
 <input type ="submit" value="Lähetä">
 </form>
 <?php 
@@ -36,14 +36,14 @@ else {
         echo "<br>Et ole täyttänyt tarvittavia kohtia<br>";
     }
     elseif ($_POST ["tunnus"]=="hello" and $_POST["salasana"]=="world"){
-        echo "<br><p>– Kani on viisas, sanoi Puh mietteliäästi, – Ja hänellä on Älyä. <br>
+        echo "<br><p>”– Kani on viisas, sanoi Puh mietteliäästi, – Ja hänellä on Älyä. <br>
         – Niin on, sanoi Nasu, – Kanilla on Älyä. <br><br>
         He olivat pitkään hiljaa... <br><br>
-        – Siitä varmaan johtuu, sanoi Puh, – että hän ei koskaan ymmärrä mitään. <br>
+        – Siitä varmaan johtuu, sanoi Puh, – että hän ei koskaan ymmärrä mitään.” <br><br>
         <i>- Nalle Puh<i><br>";
     } 
     else {
-        echo "<br>salasana tai/ja tunnus on väärin";
+        echo "<br>Salasana tai/ja tunnus on väärin";
     }}
  ?>
  </main>
