086089140a37ef0c091c0dfd37a924fd74f0656a
SagaKuutar <78218119+SagaKuutar@users.noreply.github.com>
2021-02-05 10:44:47 +0200

    Alkusekoilu

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kotitehtava</title>
+</head>
+<body>
+<div class="header">
+    <?php 'header.php'; ?>
+</div>
+
+<h1> Sisältöä </h1>
+
+<div class="footer">
+    <?php 'footer.php'; ?>
+</div>
+
+</body>
+</html>
